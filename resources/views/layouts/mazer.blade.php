fa4f323eb3a3678efd79b524f68e13c88fc89d5b
Neno Arisma <nenoarisma@gmail.com>
2023-03-31 22:25:10 +0700

    chore(login): change style of login.blade.php using layout from mazer admin as default for now

---
+++ resources/views/layouts/mazer.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="assets/css/main/app.css" />
+    <link rel="stylesheet" href="assets/css/pages/auth.css" />
+    <link rel="shortcut icon" href="assets/images/logo/favicon.svg" type="image/x-icon" />
+    <link rel="shortcut icon" href="assets/images/logo/favicon.png" type="image/png" />
+
+</head>
+
+<body>
+    <div id="auth">
+        <div class="row h-100">
+            <div class="col-lg-5 col-12">
+                <div id="auth-left">
+                    @yield('main')
+
+                </div>
+            </div>
+            <div class="col-lg-7 d-none d-lg-block">
+                <div id="auth-right"></div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
