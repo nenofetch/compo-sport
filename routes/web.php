067ec5ea0fc215d8a138a49abd387a760f231ffa
Neno Arisma <nenoarisma@gmail.com>
2023-03-31 18:40:01 +0700

    feat(auth): adding laravel ui for authentication

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/admin', function() {
     return view('admin.index');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
