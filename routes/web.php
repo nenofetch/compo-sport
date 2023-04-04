a7509ac3c190fa2b5eb5ecc9d44133195b0f6b0e
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-04-04 15:08:41 +0700

    update routes :rocket:

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,8 @@ Auth::routes();
 Route::middleware(['auth'])->group(function () {
     Route::get('dashboard', [App\Http\Controllers\Backend\DashboardController::class, 'index'])->name('dashboard');
     Route::resources([
-        'kategori' => App\Http\Controllers\Backend\CategoryController::class
+        'category' => App\Http\Controllers\Backend\CategoryController::class,
+        'articles_blog' => App\Http\Controllers\Backend\ArticlesBlogController::class,
+        'articles_news' => App\Http\Controllers\Backend\ArticlesNewsController::class,
     ]);
 });
