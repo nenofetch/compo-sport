b229e0a64ed71f0e72ccb9a9f767a7d830384128
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-04-05 18:22:26 +0700

    update frontend compro :rocket:

--- resources/views/layouts/frontend/compro/main.blade.php
+++ resources/views/layouts/frontend/compro/main.blade.php
@@ -49,12 +49,13 @@
 
         <nav id="navbar" class="navbar">
             <ul>
-                <li><a class="scrollto active" href="#hero">Home</a></li>
-                <li><a class="nav-link scrollto" href="#about">About</a></li>
-                <li><a class="nav-link scrollto" href="#services">Services</a></li>
-                <li><a class="{{Request::is('#blog') ? 'active' : ''}}" href="#portfolio">Blog</a></li>
+                <li><a class="scrollto active" href="#beranda">Beranda</a></li>
+                <li><a class="nav-link scrollto" href="#tentang">Tentang</a></li>
+                <li><a class="nav-link scrollto" href="#layanan">Layanan</a></li>
+                <li><a class="nav-link scrollto" href="#fasilitas">Fasilitas</a></li>
+                <li><a class="nav-link scrollto" href="#blog">Blog</a></li>
 
-                <li><a class="{{Request::is('booking') ? 'active' : ''}}" href="blog.html">Booking</a></li>
+                {{-- <li><a class="{{Request::is('booking') ? 'active' : ''}}" href="blog.html">Booking</a></li> --}}
                 <li><a class="getstarted" href="{{route('login')}}">Login</a></li>
             </ul>
             <i class="bi bi-list mobile-nav-toggle"></i>
@@ -140,20 +141,6 @@
             </div>
         </div>
     </div>
-
-{{--    <div class="container">--}}
-{{--        <div class="copyright">--}}
-{{--            &copy; Copyright <strong><span>FlexStart</span></strong>. All Rights Reserved--}}
-{{--        </div>--}}
-{{--        <div class="credits">--}}
-{{--            <!-- All the links in the footer should remain intact. -->--}}
-{{--            <!-- You can delete the links only if you purchased the pro version. -->--}}
-{{--            <!-- Licensing information: https://bootstrapmade.com/license/ -->--}}
-{{--            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/flexstart-bootstrap-startup-template/ -->--}}
-{{--            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-</footer><!-- End Footer -->
 
 <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
         class="bi bi-arrow-up-short"></i></a>
