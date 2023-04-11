1d97a34e39aa0fb98ec71fad6c7d2527907fb450
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-04-12 04:08:43 +0700

    update blog :rocket:

--- app/Http/Controllers/Frontend/BlogController.php
+++ app/Http/Controllers/Frontend/BlogController.php
@@ -6,25 +6,96 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Article;
 use App\Models\Category;
+use App\Models\User;
 use Conner\Tagging\Model\Tag;
+use Illuminate\Support\Facades\Hash;
 
 class BlogController extends Controller
 {
     public function index()
     {
         $articles = Article::orderBy('created_at', 'desc')->paginate(5);
+
         $categories = Category::all();
+
         $recentPosts = Article::latest()->take(5)->get();
-        $tags = Tag::pluck('name')->unique()->toArray();
-        return view('frontend.blog', compact('articles', 'categories', 'recentPosts', 'tags'));
+
+        $tags = Tag::select('name', 'slug')->distinct()->get();
+
+        return view('frontend.blog.index', compact('articles', 'categories', 'recentPosts', 'tags'));
     }
 
     public function single($slug)
     {
         $article = Article::where('slug', $slug)->firstOrFail();
+
         $categories = Category::all();
+
         $recentPosts = Article::latest()->take(5)->get();
-        $tags = Tag::pluck('name')->unique()->toArray();
-        return view('frontend.blog-single', compact('article', 'categories', 'recentPosts', 'tags'));
+
+        $tags = Tag::select('name', 'slug')->distinct()->get();
+
+        return view('frontend.blog.single', compact('article', 'categories', 'recentPosts', 'tags'));
     }
+
+    public function author($id)
+    {
+        $articles = Article::where('user_id', $id)->orderBy('created_at', 'desc')->paginate(5);
+
+        $categories = Category::all();
+
+        $recentPosts = Article::latest()->take(5)->get();
+
+        $tags = Tag::select('name', 'slug')->distinct()->get();
+
+        $author = User::findOrFail($id);
+
+        $hashedId = Hash::make($author->$id);
+
+        return view('frontend.blog.author', compact('articles', 'categories', 'recentPosts', 'tags', 'author', 'hashedId'));
+    }
+
+    public function date($date)
+    {
+        $articles = Article::whereDate('created_at', $date)->orderBy('created_at', 'desc')->paginate(5);
+
+        $categories = Category::all();
+
+        $recentPosts = Article::latest()->take(5)->get();
+
+        $tags = Tag::select('name', 'slug')->distinct()->get();
+
+        return view('frontend.blog.date', compact('articles', 'categories', 'recentPosts', 'tags', 'date'));
+    }
+
+    public function category($slug)
+    {
+        $category = Category::where('slug', $slug)->firstOrFail();
+
+        $articles = $category->article()->orderBy('created_at', 'desc')->paginate(5);
+
+        $categories = Category::all();
+
+        $recentPosts = Article::latest()->take(5)->get();
+
+        $tags = Tag::select('name', 'slug')->distinct()->get();
+
+        return view('frontend.blog.category', compact('articles', 'categories', 'recentPosts', 'tags', 'category'));
+    }
+
+    public function tag($slug)
+    {
+        $tag = Tag::where('slug', $slug)->firstOrFail();
+
+        $articles = Article::withAnyTag([$slug])->paginate(5);
+
+        $categories = Category::all();
+
+        $recentPosts = Article::latest()->take(5)->get();
+
+        $tags = Tag::select('name', 'slug')->distinct()->get();
+
+        return view('frontend.blog.tag', compact('articles', 'categories', 'recentPosts', 'tags', 'tag'));
+    }
+
 }
--- resources/views/frontend/blog/tag.blade.php
+++ resources/views/frontend/blog/tag.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.frontend.main')
 
-@section('title', 'Singgasana Sports - Detail Blog')
+@section('title', 'Singgasana Sports - Blog')
 
 @section('content')
 
@@ -13,14 +13,14 @@
             <ol>
                 <li><a href="{{ route('/') }}">Beranda</a></li>
                 <li><a href="{{ route('blog.index') }}">Blog</a></li>
-                <li>Detail Blog</li>
+                <li>Tag Blog</li>
             </ol>
-            <h2>Detail Blog</h2>
+            <h2>Blog berdasarkan tag {{ $tag->name }}</h2>
 
         </div>
     </section><!-- End Breadcrumbs -->
 
-    <!-- ======= Blog Single Section ======= -->
+    <!-- ======= Blog Section ======= -->
     <section id="blog" class="blog">
         <div class="container" data-aos="fade-up">
 
@@ -28,44 +28,60 @@
 
                 <div class="col-lg-8 entries">
 
-                    <article class="entry entry-single">
+                    @if(count($articles) > 0)
+
+                    @foreach ($articles as $row)
+                    <article class="entry">
 
                         <div class="entry-img">
-                            <img src="{{ asset('storage/article/' . $article->image) }}" alt="image-blog" class="img-fluid">
+                            <img src="{{ asset('storage/article/' . $row->image) }}" alt="image-blog" class="img-fluid">
                         </div>
 
                         <h2 class="entry-title">
-                            <a href="{{ route('blog.single', $article->slug) }}">{{ $article->title }}</a>
+                            <a href="{{ route('blog.single', $row->slug) }}">{{ $row->title }}</a>
                         </h2>
 
                         <div class="entry-meta">
                             <ul>
-                            <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a href="">{{ $article->user->name }}</a></li>
-                            <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href=""><time datetime="{{ date('d-M-Y', strtotime($article->created_at)) }}">{{ date('d-M-Y', strtotime($article->created_at)) }}</time></a></li>
-                            <li class="d-flex align-items-center"><i class="bi bi-eye"></i> <a>{{ $article->viewers }}</a></li>
+                                <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a href="{{ route('blog.author', $row->user_id) }}">{{ $row->user->name }}</a></li>
+                                <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href="{{ route('blog.date', date('y-m-d', strtotime($row->created_at))) }}"><time datetime="{{ date('y-m-d', strtotime($row->created_at)) }}">{{ date('d-M-Y', strtotime($row->created_at)) }}</time></a></li>
+                                <li class="d-flex align-items-center"><i class="bi bi-folder"></i> <a href="{{ route('blog.category', $row->category->slug) }}">{{ $row->category->title }}</a></li>
                             </ul>
                         </div>
 
                         <div class="entry-content">
-                            <p>{!! $article->content !!}</p>
+                            <p>{!! Str::limit($row->content, $limit = 700, $end = '...') !!}</p>
+                            <div class="read-more">
+                            <a href="{{ route('blog.single', $row->slug) }}">Read More</a>
+                            </div>
                         </div>
 
-                        <div class="entry-footer">
-                            <i class="bi bi-folder"></i>
-                            <ul class="cats">
-                              <li><a href="#">{{ $article->category->title }}</a></li>
-                            </ul>
-
-                            <i class="bi bi-tags"></i>
-                            <ul class="tags">
-                                @foreach ($article->tags as $tag)
-                                    <li><a href="">{{ $tag->name }}</a></li>
-                                @endforeach
-                            </ul>
-                          </div>
-
                     </article><!-- End blog entry -->
+                    @endforeach
 
+                    <div class="blog-pagination">
+                        <ul class="justify-content-center">
+                            @if ($articles->lastPage() > 1)
+                                <li class="{{ ($articles->currentPage() == 1) ? ' disabled' : '' }}">
+                                    <a href="{{ $articles->url(1) }}">Previous</a>
+                                </li>
+                                @for ($i = 1; $i <= $articles->lastPage(); $i++)
+                                    <li class="{{ ($articles->currentPage() == $i) ? ' active' : '' }}">
+                                        <a href="{{ $articles->url($i) }}">{{ $i }}</a>
+                                    </li>
+                                @endfor
+                                <li class="{{ ($articles->currentPage() == $articles->lastPage()) ? ' disabled' : '' }}">
+                                    <a href="{{ $articles->url($articles->currentPage()+1) }}">Next</a>
+                                </li>
+                            @endif
+                        </ul>
+                    </div>
+
+                    @else
+                        <div class="entry-content">
+                            <h2 class="entry-title">Tidak ada artikel yang ditemukan.</h2>
+                        </div>
+                    @endif
                 </div><!-- End blog entries list -->
 
                 <div class="col-lg-4">
@@ -84,7 +100,7 @@
                         <div class="sidebar-item categories">
                             <ul>
                                 @foreach ($categories as $row)
-                                <li><a href="#">{{ $row->title }} <span>({{ count($row->article) }})</span></a></li>
+                                <li><a href="{{ route('blog.category', $row->slug) }}">{{ $row->title }} <span>({{ count($row->article) }})</span></a></li>
                                 @endforeach
                             </ul>
                         </div><!-- End sidebar categories-->
@@ -105,7 +121,7 @@
                         <div class="sidebar-item tags">
                             <ul>
                                 @foreach ($tags as $tag)
-                                <li><a href="">{{ $tag }}</a></li>
+                                <li><a href="{{ route('blog.tag', $tag->slug) }}">{{ $tag->name }}</a></li>
                                 @endforeach
                             </ul>
                         </div><!-- End sidebar tags-->
