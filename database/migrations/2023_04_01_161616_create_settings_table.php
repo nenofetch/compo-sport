1b81fe09cada74c40ddf927d521770446629f98a
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-04-13 22:13:17 +0700

    update migration :rocket:

--- database/migrations/2023_04_01_161616_create_settings_table.php
+++ database/migrations/2023_04_01_161616_create_settings_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->string('hero');
+            $table->string('favicon');
             $table->string('slogan');
             $table->string('visitors');
             $table->string('event');
