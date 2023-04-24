8020af3a19136b04fa4772c5f066e44f55b5e38c
Neno Arisma <nenoarisma@gmail.com>
2023-04-25 05:27:10 +0700

    chore: adding popper.js adding stack for some page when it needed on some page

--- resources/views/layouts/frontend/main.blade.php
+++ resources/views/layouts/frontend/main.blade.php
@@ -6,12 +6,14 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <title>{{ $setting->name }}</title>
-    <meta name="description" content="Singgasana Sports and Recreation Centre Adalah sarana olah raga dan rekreasi keluarga terletak di kawasan exclusive Permukiman Singgasana Pradana – Bandung. Terdapat fasilitas olahraga dan sarana rekreasi untuk warga sekitar & masyarakat luas. Fasilitas tersebut antara lain : Lapangan Tenis Indoor, Basket Indoor, Squash, Tenis Meja, Badminton, Batting Practice, Fitness, Aerobic, Steam, Whirlpool, Jujitsu, Archery, Sport Shop, Swimming Pool, Auditorium, Function Room, dan Pool Side Cafe. " />
-    <meta name="keywords" content="singgasana, sports, recreation center, rekreasi, olahraga, tempat olahraga, rekreasi keluar, beladiri, bandung, jawa barat, tenis indoor, renang, basket indoor, xquash, tenis meja, badminton, batting practice, fitness, aerobic, steam, whirlpool, jujitsu, archery, sport shop, swimming pool, auditorium, function room, pool side cafe." />
-    <meta name="author" content="Singgasana Sports and Recreation Centre" />
-    <meta name="email" content="singgasanasnr@gmail.com" />
-    <meta name="website" content="http://singgasanasports-recreationcentre.com" />
-    <meta name="Version" content="v1.0.0" />
+    <meta name="description"
+          content="Singgasana Sports and Recreation Centre Adalah sarana olah raga dan rekreasi keluarga terletak di kawasan exclusive Permukiman Singgasana Pradana – Bandung. Terdapat fasilitas olahraga dan sarana rekreasi untuk warga sekitar & masyarakat luas. Fasilitas tersebut antara lain : Lapangan Tenis Indoor, Basket Indoor, Squash, Tenis Meja, Badminton, Batting Practice, Fitness, Aerobic, Steam, Whirlpool, Jujitsu, Archery, Sport Shop, Swimming Pool, Auditorium, Function Room, dan Pool Side Cafe. "/>
+    <meta name="keywords"
+          content="singgasana, sports, recreation center, rekreasi, olahraga, tempat olahraga, rekreasi keluar, beladiri, bandung, jawa barat, tenis indoor, renang, basket indoor, xquash, tenis meja, badminton, batting practice, fitness, aerobic, steam, whirlpool, jujitsu, archery, sport shop, swimming pool, auditorium, function room, pool side cafe."/>
+    <meta name="author" content="Singgasana Sports and Recreation Centre"/>
+    <meta name="email" content="singgasanasnr@gmail.com"/>
+    <meta name="website" content="http://singgasanasports-recreationcentre.com"/>
+    <meta name="Version" content="v1.0.0"/>
 
     <!-- Favicons -->
     <link href="{{ asset('storage/setting/' . $setting->favicon) }}" rel="icon">
@@ -57,22 +59,26 @@
                 <li><a class="nav-link scrollto active" href="{{ route('/') }}">Beranda</a></li>
                 <li class="dropdown"><a href="#"><span>Profil</span> <i class="bi bi-chevron-down"></i></a>
                     <ul>
-                      <li><a href="{{ route('pages.index', 'tentang-kami') }}">Tentang Kami</a></li>
-                      <li class="dropdown"><a href=""><span>Aktifitas</span> <i class="bi bi-chevron-right"></i></a>
-                        <ul>
-                          <li><a href="{{ route('pages.index', 'singgasana-swimming-club') }}">Singgasana Swimming Club</a></li>
-                          <li><a href="{{ route('pages.index', 'aerobik-seni-bela-diri') }}">Aerobik & Seni Bela Diri</a></li>
-                        </ul>
-                      </li>
-                      <li><a href="{{ route('gallery.index') }}">Galeri</a></li>
+                        <li><a href="{{ route('pages.index', 'tentang-kami') }}">Tentang Kami</a></li>
+                        <li class="dropdown"><a href=""><span>Aktifitas</span> <i class="bi bi-chevron-right"></i></a>
+                            <ul>
+                                <li><a href="{{ route('pages.index', 'singgasana-swimming-club') }}">Singgasana Swimming
+                                        Club</a></li>
+                                <li><a href="{{ route('pages.index', 'aerobik-seni-bela-diri') }}">Aerobik & Seni Bela
+                                        Diri</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="{{ route('gallery.index') }}">Galeri</a></li>
                     </ul>
-                  </li>
+                </li>
                 </li>
                 <li class="dropdown"><a href=""><span>Fasilitas</span> <i class="bi bi-chevron-down"></i></a>
                     <ul>
                         <li><a href="{{ route('facilities.index', 'whirlpool-steam') }}">Whirlpool & Steam</a></li>
-                        <li><a href="{{ route('facilities.index', 'auditorium-function-room') }}">Auditorium & Function Room</a></li>
-                        <li><a href="{{ route('facilities.index', 'pool-side-cafe-kantin') }}">Pool Side Café & Kantin</a></li>
+                        <li><a href="{{ route('facilities.index', 'auditorium-function-room') }}">Auditorium & Function
+                                Room</a></li>
+                        <li><a href="{{ route('facilities.index', 'pool-side-cafe-kantin') }}">Pool Side Café &
+                                Kantin</a></li>
                         <li><a href="{{ route('facilities.index', 'aerobik') }}">Aerobik</a></li>
                     </ul>
                 </li>
@@ -126,7 +132,8 @@
                     <h4>Tautan</h4>
                     <ul>
                         <li><i class="bi bi-chevron-right"></i> <a href="{{ route('/') }}">Beranda</a></li>
-                        <li><i class="bi bi-chevron-right"></i> <a href="{{ route('pages.index', 'tentang-kami') }}">Tentang Kami</a></li>
+                        <li><i class="bi bi-chevron-right"></i> <a href="{{ route('pages.index', 'tentang-kami') }}">Tentang
+                                Kami</a></li>
                         <li><i class="bi bi-chevron-right"></i> <a href="{{ route('blog.index') }}">Blog</a></li>
                         <li><i class="bi bi-chevron-right"></i> <a href="#fasilitas">Keanggotaan</a></li>
                         <li><i class="bi bi-chevron-right"></i> <a href="{{ route('contact.index') }}">Kontak</a></li>
@@ -136,10 +143,16 @@
                 <div class="col-lg-2 col-6 footer-links">
                     <h4>Fasilitas</h4>
                     <ul>
-                        <li><i class="bi bi-chevron-right"></i><a href="{{ route('facilities.index', 'whirlpool-steam') }}">Whirlpool & Steam</a></li>
-                        <li><i class="bi bi-chevron-right"></i><a href="{{ route('facilities.index', 'auditorium-function-room') }}">Auditorium & Function Room</a></li>
-                        <li><i class="bi bi-chevron-right"></i><a href="{{ route('facilities.index', 'pool-side-cafe-kantin') }}">Pool Side Café & Kantin</a></li>
-                        <li><i class="bi bi-chevron-right"></i><a href="{{ route('facilities.index', 'aerobik') }}">Aerobik</a></li>
+                        <li><i class="bi bi-chevron-right"></i><a
+                                href="{{ route('facilities.index', 'whirlpool-steam') }}">Whirlpool & Steam</a></li>
+                        <li><i class="bi bi-chevron-right"></i><a
+                                href="{{ route('facilities.index', 'auditorium-function-room') }}">Auditorium & Function
+                                Room</a></li>
+                        <li><i class="bi bi-chevron-right"></i><a
+                                href="{{ route('facilities.index', 'pool-side-cafe-kantin') }}">Pool Side Café &
+                                Kantin</a></li>
+                        <li><i class="bi bi-chevron-right"></i><a href="{{ route('facilities.index', 'aerobik') }}">Aerobik</a>
+                        </li>
                     </ul>
                 </div>
 
@@ -148,8 +161,8 @@
                     <p>
                         {{ $setting->address1 }}
                         <strong>Phone:</strong>
-                        <p>P : {{ $setting->telephone1 }} <br>F : {{ $setting->telephone2 }}</p><br>
-                        <strong>Email:</strong> {{ $setting->email }}<br>
+                    <p>P : {{ $setting->telephone1 }} <br>F : {{ $setting->telephone2 }}</p><br>
+                    <strong>Email:</strong> {{ $setting->email }}<br>
                     </p>
 
                 </div>
@@ -159,20 +172,29 @@
     </div>
 </footer>
 
-    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
-            class="bi bi-arrow-up-short"></i></a>
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+        class="bi bi-arrow-up-short"></i></a>
 
-    <!-- Vendor JS Files -->
-    <script src="{{ asset('frontend') }}/assets/vendor/purecounter/purecounter_vanilla.js"></script>
-    <script src="{{ asset('frontend') }}/assets/vendor/aos/aos.js"></script>
-    <script src="{{ asset('frontend') }}/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="{{ asset('frontend') }}/assets/vendor/glightbox/js/glightbox.min.js"></script>
-    <script src="{{ asset('frontend') }}/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-    <script src="{{ asset('frontend') }}/assets/vendor/swiper/swiper-bundle.min.js"></script>
-    <script src="{{ asset('frontend') }}/assets/vendor/php-email-form/validate.js"></script>
+<!-- Vendor JS Files -->
+<script src="{{ asset('frontend') }}/assets/vendor/purecounter/purecounter_vanilla.js"></script>
+<script src="{{ asset('frontend') }}/assets/vendor/aos/aos.js"></script>
+<script src="{{ asset('frontend') }}/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="{{ asset('frontend') }}/assets/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="{{ asset('frontend') }}/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="{{ asset('frontend') }}/assets/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="{{ asset('frontend') }}/assets/vendor/php-email-form/validate.js"></script>
 
-    <!-- Template Main JS File -->
-    <script src="{{ asset('frontend') }}/assets/js/main.js"></script>
+<!-- Template Main JS File -->
+<script src="{{ asset('frontend') }}/assets/js/main.js"></script>
+
+
+<!-- Popper JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"
+        integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE"
+        crossorigin="anonymous"></script>
+
+<!-- Using this while rendering some javascript on some page -->
+@stack('script')
 
 </body>
 
