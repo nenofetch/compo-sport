559c0ebc0f9701398cde6aba4c3a7599d1f4429c
Neno Arisma <nenoarisma@gmail.com>
2023-05-05 15:24:15 +0700

    chore: request file of csv MembershipController

---
+++ app/Http/Controllers/Frontend/MembershipController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use App\Models\Memberships;
+use Illuminate\Http\Request;
+
+class MembershipController extends Controller
+{
+    public function storePersonal(Request $request)
+    {
+
+
+        $personalMembership = $request->validate([
+            'nama_lengkap' => 'required',
+            'tipe_membership' => 'required',
+            'email' => 'required|email:dns',
+            'notelp' => 'required|numeric',
+            'membership' => 'required',
+            'alamat' => 'required'
+        ]);
+
+        Memberships::create($personalMembership);
+        return redirect('/')->with('message', 'Data Berhasil Dikirim. Silahkan Lihat Email Konfirmasi');
+    }
+
+    public function storeCouple(Request $request)
+    {
+        $coupleMembership = $request->validate([
+            'nama_lengkap' => 'required',
+            'nama_penanggung' => 'required',
+            'tipe_membership' => 'required',
+            'email' => 'required|email:dns',
+            'notelp' => 'required|numeric',
+            'membership' => 'required',
+            'alamat' => 'required',
+        ]);
+
+        Memberships::create($coupleMembership);
+        return redirect('/')->with('message', 'Data Berhasil Dikirim. Silahkan Lihat Email Konfirmasi');
+    }
+
+    public function storeTriple(Request $request)
+    {
+        $tripleMembership = $request->validate([
+            'nama_lengkap' => 'required',
+            'nama_penanggung' => 'required',
+            'tipe_membership' => 'required',
+            'email' => 'required|email:dns',
+            'notelp' => 'required|numeric',
+            'membership' => 'required',
+            'alamat' => 'required',
+            'csv' => 'required|file|mimes:csv'
+        ]);
+
+        if ($request->hasFile('csv')) {
+            $tripleMembership['csv'] = $request->file('csv')->store('data');
+        }
+
+        Memberships::create($tripleMembership);
+        return redirect('/')->with('message', 'Data Berhasil Dikirim. Silahkan Lihat Email Konfirmasi');
+    }
+
+    public function storeFamily(Request $request)
+    {
+        $familyMembership = $request->validate([
+            'nama_lengkap' => 'required',
+            'nama_penanggung' => 'required',
+            'tipe_membership' => 'required',
+            'email' => 'required|email:dns',
+            'notelp' => 'required|numeric',
+            'membership' => 'required',
+            'alamat' => 'required',
+            'csv' => 'required|file|mimes:csv'
+        ]);
+
+        Memberships::create($familyMembership);
+        return redirect()->route('/');
+    }
+
+    public function storeStudent(Request $request)
+    {
+        $studentMembership = $request->validate([
+            'nama_lengkap' => 'required',
+            'nama_penanggung' => 'required',
+            'tipe_membership' => 'required',
+            'email' => 'required|email:dns',
+            'notelp' => 'required|numeric',
+            'membership' => 'required',
+            'alamat' => 'required',
+            'csv' => 'required|file|mimes:csv'
+        ]);
+
+        Memberships::create($studentMembership);
+        return redirect('/')->with('message', 'Data Berhasil Dikirim. Silahkan Lihat Email Konfirmasi');
+    }
+
+    public function storeCommunity(Request $request)
+    {
+        $communityMembership = $request->validate([
+            'nama_lengkap' => 'required',
+            'nama_penanggung' => 'required',
+            'tipe_membership' => 'required',
+            'email' => 'required|email:dns',
+            'notelp' => 'required|numeric',
+            'membership' => 'required',
+            'alamat' => 'required',
+            'csv' => 'required|file|mimes:csv'
+        ]);
+
+        Memberships::create($communityMembership);
+        return redirect('/')->with('message', 'Data Berhasil Dikirim. Silahkan Lihat Email Konfirmasi');
+    }
+}
